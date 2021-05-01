a657f107c637f9c2e82ed7901cde3e4ea150ef26
Irina Kvitkina <irina.kvitkina@gmail.com>
2021-05-01 23:41:46 +0300

    array

---
+++ src/scripts/Card.js
@@ -0,0 +1,51 @@
+export class Card {
+  constructor({ data }, elementsTemplate) {
+    this._photo = data.photo
+    this._name = data.name
+    this._status = data.status
+    this._color = data.color
+    this._flag = data.flag
+    this._country = data.country
+    this._date = data.date
+    this._star = data.star
+    this._comments = data.comments
+    this._company = data.company
+    this._plus = data.plus
+    this._minus = data.minus
+    this._up = data.up
+    this._down = data.down
+    this._elementsTemplate = elementsTemplate
+  }
+
+  _getTemplate () {
+    const cardElement = document.querySelector(this._elementsTemplate).content.querySelector('.review').cloneNode(true)
+    return cardElement
+  }
+
+  generateCard () {
+    this._element = this._getTemplate()
+    const status = this._element.querySelector('.review__status')
+    console.log(this._flag)
+    this._element.querySelector('.review__person-photo').style.backgroundImage = this._photo
+    this._element.querySelector('.review__name').textContent = this._name
+    status.textContent = this._status
+    this._element.querySelector('.review__country-icon').src = this._flag
+    this._element.querySelector('.review__text_country').textContent = this._country
+    this._element.querySelector('.review__caption_date').textContent = this._date
+    this._element.querySelector('.review__rating-value').textContent = this._star
+    this._element.querySelector('.review__caption_comments').textContent = this._comments
+    this._element.querySelector('.review__text_company').textContent = this._company
+    this._element.querySelector('.review__comments-text_plus').textContent = this._plus
+    this._element.querySelector('.review__comments-text_minus').textContent = this._minus
+    this._element.querySelector('.review__thumb-value_up').textContent = this._up
+    this._element.querySelector('.review__thumb-value_down').textContent = this._down
+    
+    if(this._color === 'red'){
+      status.classList.add('review__status_blocked')
+    } else {
+      status.classList.remove('review__status_blocked')
+    }
+    
+    return this._element
+  }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,61 +1,22 @@
-import '../styles/index.scss';
+import '../pages/index.scss';
+import { Card } from './Card';
+import { Section } from './Section';
+import { array } from './array';
+const elementsList = document.querySelector('.reviews__list');
 
-//drop-select
-const dropSelects = document.querySelectorAll('.form__item_type_select');
+//создание карточки
+const createCardFunction = (data) => {
+    const card = new Card({data,
+    }, '.reviews__template')
+    const element = card.generateCard()
+    cardsList.addItem(element)
+  }
+  
+  //создание экземпляра класса для отрисовки карточек на странице
+  const cardsList = new Section ({
+    renderer: (data => {
+    createCardFunction(data)
+    })
+  }, elementsList)
 
-const selectInit = (elem) => {
-  const formInput = elem.querySelector('.form__input');
-  const formList = elem.querySelector('.form__list');
-  const formListItems = formList.querySelectorAll('.form__list-item');
-  const formInputIcon = elem.querySelector('.form__input-icon');
-
-  elem.addEventListener('click', () => {
-      formList.classList.toggle('form__list_visible');
-      formInputIcon.classList.toggle('form__input-icon_rotate');
-  });
-
-  formListItems.forEach(item => {
-    item.addEventListener('click', () => {
-      formInput.value = item.textContent;
-    });
-  });
-};
-
-for (let item of dropSelects) {
-  selectInit(item);
-};
-
-//checkbox
-const formCheckbox = document.querySelector('.form__checkbox-content');
-const formCheckboxIcon = formCheckbox.querySelector('.form__checkbox-icon');
-const formCheckboxInput = formCheckbox.querySelector('.form__checkbox-input');
-const formTemperature = document.querySelector('.form__temperature');
-
-formCheckbox.addEventListener('click', () => {
-  formCheckboxIcon.classList.toggle('form__checkbox-icon_visible');
-  formCheckbox.classList.toggle('form__checkbox-content_checked');
-  formTemperature.classList.toggle('form__temperature_visible');
-});
-
-formCheckboxIcon.addEventListener('click', () => {
-  formCheckboxIcon.classList.remove('form__checkbox-icon_visible');
-  formCheckbox.classList.remove('form__checkbox-content_checked');
-  formTemperature.classList.remove('form__temperature_visible');
-});
-
-//time
-const formTime = document.querySelectorAll('.form__time-wrapper');
-
-const selectTime = (elem) => {
-  const formInputButton = elem.querySelector('.form__input-button');
-  const formInputsCircle = elem.querySelector('.form__input-circle');
-
-  formInputsCircle.addEventListener('click', () => {
-    formInputsCircle.classList.toggle('form__input-circle_active');
-    formInputButton.classList.toggle('form__input-button_active');
-  })
-};
-
-for (let item of formTime) {
-  selectTime(item);
-};
+  cardsList.renderItems(array)
